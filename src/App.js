a03d898a93ee8731bd81d5659b437c3811073117
malaury <malaury.lemaitresalmon@gmail.com>
2022-12-23 12:45:37 +0100

    change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>En dev ...</p>
+        <p>En dev yoyo</p>
       </header>
     </div>
   );
