86203d2909d2fcc685344f5581688569eb7d41fc
malaury <malaury.lemaitresalmon@gmail.com>
2022-12-23 16:16:29 +0100

    test no branch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>En dev loth ...</p>
+        <p>En dev loth ... block branch</p>
       </header>
     </div>
   );
